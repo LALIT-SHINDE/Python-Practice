08af24b19b971602955a1d6fe5020a7b7d6c82c0
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-08-04 23:09:56 +0530

    Update 1.py

--- Day 9/1.py
+++ Day 9/1.py
@@ -3,14 +3,15 @@ count = 0
 
 for i in range(2, n+1):
     prime = True
-
-    for j in range(2, i):
-        if i % j == 0:
+    
+    for j in range(2, n):
+         if i % j == 0:
             prime = False
             break
-
     if prime:
-        print(i)
-        count += 1
-
+        prime = True
+        count =+ 1
 print(count)
+
+
+
