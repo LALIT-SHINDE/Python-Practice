7b39d085ac40bd1bd539a0f2de62561de765f660
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-08-04 23:15:50 +0530

    Update 2.py

--- Day 9/2.py
+++ Day 9/2.py
@@ -1,11 +1,10 @@
-n = int(input("Enter the number: "))
+n = int(input("Enter any number"))
 fact = 1
 total = 0
+for i in range(2, n+1):
+    fact *= i
+    total += fact
 
-for i in range(1, n+1):
-    fact = fact * i
-    print(f"{i}! = {fact}")
-
-    total = total + fact
-
-print(f"The Sum of the factorial of 1 to {n} : {total}")
+print(f"{n}! = {fact}")
+print(total)
+    
