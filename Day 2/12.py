8c169403e531ea56c0bfa1be8d17ab0c3e26f7ba
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-08-14 23:31:04 +0530

    Add output examples for voting eligibility
    Added output examples for voting eligibility cases.

--- Day 2/12.py
+++ Day 2/12.py
@@ -5,3 +5,20 @@ if citizenship == "indian" and age >= 18:
     print("Eligible for voting")
 else:
     print("Not Eligible")
+
+# Output
+
+# Case 1
+# Enter Your Citizenship: Indian
+# Enter your age: 32
+# Eligible for voting
+
+# Case 2
+# Enter Your Citizenship: Indian
+# Enter your age: 12
+# Not Eligible
+
+# Case 3
+# Enter Your Citizenship: USA
+# Enter your age: 32
+# Not Eligible
