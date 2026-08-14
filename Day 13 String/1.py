2277019e27c130a4f17d46899a074bfb2a78d50c
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-08-14 20:26:46 +0530

    Update string manipulation examples with loops
    Refactor and add loops to various string manipulations.

--- Day 13 String/1.py
+++ Day 13 String/1.py
@@ -1,120 +1,72 @@
-#1
-text = "Python Programming"
-print(text)
+n = 17
+j = 4
+for i in range(1, n):
+    if i == 11:
+        break
+     
+    print(f"{j} * {i} = {i*n}")
 
-#2
-txet = "Python"
-print(text[:1])
-# or
-print(text[0])
+print("End.....")
 
-#3
-txt = "Developer"
-print(txt[-1])
 
-#4 
-txt = "computer"
-print(txt[2])
+for i in range(0, 12):
+    if i == 0 or i == 11 :
+        continue
 
-#5
-language = "JavaScript"
-print(len(language))
-
-#6
-text = "python"
-if "thon" in text:
-    print(text, True)
-
-#7
-text = "Python"
-if "Java" not in text:
-    print(text)
-
-#8
-first = "Hello"
-second = "World"
-print(first + second)
-
-#9
-text = "python"
-for i in range(5):
-    print(text)
-
-#10
-txt = input("Enter your name: ")
-print(len(txt))
-
-#11
-txt = "\nProgramming"
-for i in txt:
     print(i)
 
-#12
-txt = "Mumbai"
-print(txt[-2])
+for i in range(1, 6):
+    if i == 3:
+        continue
 
-#13
-fruit = "Watermelon"
-print(fruit[4:6])
+    print(i)
 
-#14
-name = "saturday"
-print(name[0])
-print(name[1])
-print(name[2])
-print(name[3])
-print(name[4])
-print(name[5])
-print(name[6])
-print(name[7])
 
-#15 
+for i in range(1,11):
+    if i == 7:
+        break
+    print(i)
 
-# Predict the output:
-# text = "Python"
-# print(text[-1])             n
-# print(text[-3])             h
-# print(text[0])              P
-# print(text[5])              n
+for i in range(1,21):
+    if i == 3:
+        continue
+    print(f"{n} * {i} = {n*i}")
 
-#16
-text = "PythonProgramming"
-print(text[:6])
-print(text[6:])
 
-#17
-print(text[0])
-print(text[2])
-print(text[4])
-print(text[6])
-print(text[7])
-print(text[8])
-print(text[9])
-print(text[10])
-print(text[12])
-print(text[14])
-print(text[16])
+# while True:
+#     num = input("Enter the Name: ")
 
-#19
-text = "PythonProgramming"
-a = len(text)
-print(text[a::-1])
+#     print(f"Your Name: {num}")
 
-#20
-print(text[0:6])
+#     if num == "Skip":
+#         print(f"Quit: {num}")
+#         break
 
-#21
-print(text[-5:])
+c =0
+while c != 3:
+    print("--- Menu ---")
+    print("1. say Hello.")
+    print("2. Show Time ")
+    print("3. Exit")
 
-#22
-print(text[2:8])
+    c = int(input("Enter choice: "))
+    match c:
+        case 1:
+            print("Hello, Jarvis")
 
-#23
-print(text[0::2])
+        case 2:
+            print("Feature coming soon... ")
+            pass
 
-#24
-print(text[1:])
+        case 3: 
+            print("Byy....")
+            break
 
-#25
+        case _ :
+            print("Invaild Choice")
+            pass
+      
 
-print(text[0:-1])
+
+
+    
