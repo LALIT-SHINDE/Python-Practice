514182aa684e9152af528db4fee1166e3259216d
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-08-14 23:27:02 +0530

    Update 1.py

--- Day 2/1.py
+++ Day 2/1.py
@@ -9,3 +9,13 @@ print(a," // ",b," = ",a//b)
 print(a," % ",b," = ",a%b)
 print(a," ** ",b," = ",a**b)
 
+# Output
+# Enter the value of a: 4
+# Enter the value of b: 5
+# 4  +  5  =  9
+# 4  -  5  =  -1
+# 4  *  5  =  20
+# 4  /  5  =  0.8
+# 4  //  5  =  0
+# 4  %  5  =  4
+# 4  **  5  =  1024
