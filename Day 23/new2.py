0ce677952d53eb7f97b273deac7940c224ab7e4d
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-08-21 21:22:01 +0530

    Update new2.py

--- Day 23/new2.py
+++ Day 23/new2.py
@@ -34,3 +34,20 @@ x = {
     }
 
 print(json.dumps(x, sort_keys = True))
+
+# Output
+# {'Name': 'Lalit', 'Age': 22, 'City': 'Nashik'}
+# Lalit
+# 22
+# Nashik
+# {'Name': 'Lalit', 'Age': 22, 'City': 'Nashik'} <class 'dict'>
+# {"Name": "Lalit", "Age": 22, "City": "Nashik"} <class 'str'>
+# [12, 3, 24, 54, 6, 64, 7, 8, 9]
+# [43, 56, 6, 4, 3, 5]
+# "Lalit"
+# 43
+# 32.43
+# true
+# false
+# null
+# {"Age": 22, "Cars": [{"Model": "Mercedes", "mpg": 542}], "Childern": ["Billy", "Tommy"], "Divorced": false, "Married": true, "Name": "Lalit", "Pets": null}
