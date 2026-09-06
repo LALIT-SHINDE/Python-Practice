e0595bb5146e7591bd332943ba584fecb10ebdff
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-06 21:28:53 +0530

    Update New1.py

--- Day 28/New1.py
+++ Day 28/New1.py
@@ -1,1 +1,150 @@
 
+# 30. Final Challenge — Combine Everything
+# ===== STUDENT MANAGEMENT SYSTEM =====
+
+# 1. Add Student
+# 2. Display Students
+# 3. Calculate Total
+# 4. Calculate Average
+# 5. Find Highest Marks
+# 6. Find Lowest Marks
+# 7. Check Pass/Fail
+# 8. Display Grade
+# 9. Exit
+
+students = {
+    "Amit": [78, 85, 67, 90, 72],
+    "Priya": [88, 92, 79, 85, 90]
+}
+
+def add_student(dics, name, marks):
+       
+        if len(marks) == 5:
+            dics[name] =  marks
+            return f"Added."
+
+        else:
+            return f"Invaild Entry"
+
+def total(stu):
+    for name, marks in stu.items():
+        total = 0
+
+        for i in marks:
+            total += i
+        print(f"{name} : Total Marks :{total}")
+
+def average(stu):
+    for name, marks in stu.items():
+        total = 0
+
+        for i in marks:
+            total += i
+
+        average = total / len(marks)
+        print(f"{name} : Average marks :{average}")
+
+def highest_marks(stu):
+    for name, marks in stu.items():
+        maxi = marks[0]
+
+        for i in marks:
+            if maxi < i:
+                maxi = i
+        print(f"{name} : Highest Marks: {maxi}")
+
+def lowest_marks(stu):
+    for name, marks in stu.items():
+        mini = marks[0]
+
+        for i in marks:
+            if mini > i:
+                mini = i
+
+        print(f"{name}: Lowest marks: {mini}")
+
+def result(stu):
+    for name, marks in stu.items():
+        total = 0
+
+        for i in marks:
+            total += i
+
+        average = total / len(marks)
+        if average <= 40:
+            print(f"{name} : Fail")
+        else:
+            print(f"{name} : pass")
+
+def grade(stu):
+    for name, marks in stu.items():
+        total = 0
+
+        for i in marks:
+            total += i
+
+        ave = total/len(marks)
+
+        if ave <= 100 and ave >= 91:
+            grade = "A+"
+
+        elif ave <= 90 and ave >= 81:
+            grade = "A"
+
+        elif ave <= 80 and ave >= 71:
+            grade = "B"
+
+        elif ave <= 70 and ave >= 56:
+            grade = "C"
+
+        elif ave <= 55 and ave >= 40:
+            grade = "D"
+
+        else:
+            grade = "F"
+
+        print(grade)
+
+while True:
+    print("\n===== STUDENT MANAGEMENT SYSTEM =====\n1. Add Student\n2. Display Students\n3. Calculate Total")
+    print("4. Calculate Average\n5. Find Highest Marks\n6. Find Lowest Marks\n7. Check Pass/Fail\n8. Display Grade\n9. Exit")
+
+    choice = int(input("Enter the Choice: "))
+
+    match choice:
+        case 1:
+            name = input("\nEnter the name: ")
+            marks = list(map(int, input("Enter the 5 Marks: ").split()))
+
+            print(add_student(students, name,marks))
+
+        case 2:
+            for name, marks in students.items():
+                print(f"{name} = {marks}")
+
+        case 3: 
+            total(students)
+
+        case 4:
+            average(students)
+
+        case 5:
+            highest_marks(students)
+
+        case 6:
+            lowest_marks(students)
+
+        case 7:
+            result(students)
+
+        case 8: 
+            grade(students)
+
+        case 9:
+            print("Exit.....")
+            break
+
+        case _:
+            print("Invalid choice, Try again....")
+
+            
