d4b4b5dd48048cea9a6b90ad41493ecc93924e97
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-07 21:45:01 +0530

    Update New1.py

--- Day 29/New1.py
+++ Day 29/New1.py
@@ -1,1 +1,24 @@
+class Myclass:
+    x = 10
 
+class new:
+    pass
+    
+a = Myclass()
+b = Myclass 
+c = Myclass
+
+print(a.x)
+print(b.x)
+print(c.x)
+print(a)
+
+del a 
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+obj = Person("Jhon",32)
+print(obj.name, obj.age)
