c8a3b49b38235bfc6febd1607731a437c6e7c252
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-07 21:44:18 +0530

    Update New.py

--- Day 29/New.py
+++ Day 29/New.py
@@ -1,1 +1,29 @@
+#1
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+s1 = Student("Amit", 21)
+print(s1.name, s1.age)
+
+#2
+class Car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+c1 = Car("BMW","Black")
+print(c1.brand, c1.color)
+
+#3
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print(f"Hello, I am {self.name}")
+
+s1 = Person("John")
+s1.greet()
 
