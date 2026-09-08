ba10f116d7e4ad5acf407de489cb092738f34512
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-08 21:23:56 +0530

    Update New.py

--- Day 30/New.py
+++ Day 30/New.py
@@ -1,1 +1,15 @@
+class person:
+    def __init__(self, name, age ,city,goal):
+        self.name = name
+        self.age = age
+        self.city = city
+        self.goal = goal
 
+    def show(self):
+        print(f"Name: {self.name}\nCity: {self.city}\nGoal: {self.goal}\nAge: {self.age}\n")
+
+s = person("amit",23,"Nashik","gg")
+s.show()
+
+p = person("sumit",21,"Pune","ff")
+p.show()
