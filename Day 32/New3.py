12821ac4d53d7d7315f592d5b665b2137c1369e0
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-11 21:15:53 +0530

    Implement person and Student classes
    Add person and Student classes with initialization and print methods.

--- Day 32/New3.py
+++ Day 32/New3.py
@@ -1,1 +1,17 @@
 
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def printname(self):
+        print(f"{self.name} ({self.age})")
+
+class Student(person):
+    def __init__(self, course):
+        self.course = course
+
+    def ko(self):
+        print(f"{self.name} ({self.age}) {self.course}")
+    
+    
