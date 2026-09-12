e148de67bbd4f0a28e64c0b91dc67b6de83a8e59
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-12 20:41:31 +0530

    Update new2.py

--- Day 33/new2.py
+++ Day 33/new2.py
@@ -1,1 +1,17 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
+  def show(self):
+    print(self.name, self.age)
+
+class student(Person):
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age 
+
+    super().__init__(name, age)
+
+s1 = student("Sumit", 23)
+s1.show()
