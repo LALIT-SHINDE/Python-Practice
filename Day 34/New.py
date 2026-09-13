4b58057653201bba42d635ecb4bf5b20e1d8fc59
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-13 20:47:27 +0530

    Update New.py

--- Day 34/New.py
+++ Day 34/New.py
@@ -1,1 +1,14 @@
+n = int(input("Enter the number: "))
+if n <= 1:
+  prime = False
 
+else:
+  prime = True
+  for i in range(2, n):
+    if n % i == 0:
+      prime = False
+      break
+if prime:
+  print(f"{n} is a Prime Number.")
+else:
+  print(f"{n} is not a Prime Number.")
