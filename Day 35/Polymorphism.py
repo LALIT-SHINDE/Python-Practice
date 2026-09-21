f038d815e7566e860f91dcd6e94a5ac7b76068f7
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-21 18:33:50 +0530

    Update Polymorphism.py

--- Day 35/Polymorphism.py
+++ Day 35/Polymorphism.py
@@ -1,1 +1,24 @@
+class Vehical:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
 
+class Car(Vehical):
+    def move(self):
+        print(self.brand, self.model,"Dive !!")
+
+class Boat(Vehical):
+    def move(self):
+        print(self.brand, self.model, "Sail !!")
+
+class Palne(Vehical):
+    def move(self):
+        print(self.brand, self.model, "Fly !!")
+
+c1 = Car("BMW", "M06")
+b1 = Boat("Titan","m32")
+p1 = Palne("Air Jet", "New")
+
+c1.move()
+b1.move()
+p1.move()
