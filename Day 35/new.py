7ae1b962b7dfd528225a8c7e1dffbe95d1ac8b2e
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-21 18:32:07 +0530

    Update new.py

--- Day 35/new.py
+++ Day 35/new.py
@@ -1,1 +1,38 @@
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.__age = age
+
+#     def get(self):
+#         print(f"Name : {self.name}\nAge : {self.__age}")
+
+#     def set(self, age):
+#         if self.__age < 18:
+#             self.__age = age
+
+
+# p1 = Person("Lalit", 42)
+# p1.get()
+# p1.set(23)
+# p1.get()
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age
+
+    def show(self):
+        print(self.name, self.__age)
+
+    def set(self, age):
+        if age > 18:
+            self.__age = age
+
+        else:
+            print(F"age should be greater than 18")
+
+s1 = Student("Lalit", 12)
+s1.set(43)
+s1.show()
 
