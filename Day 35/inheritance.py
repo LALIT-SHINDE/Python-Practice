6e1c5727c21abeaf2d835ed5b9eb8a19e523d735
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-09-21 18:33:02 +0530

    Update inheritance.py

--- Day 35/inheritance.py
+++ Day 35/inheritance.py
@@ -1,1 +1,18 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    def show(self):
+        print(f"{self.name} {self.age}")
+
+class Student(Person):
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age 
+        self.course = course
+
+        print(self.course)
+
+s1 = Student("lalit", 22, "Mca")
+s1.show()
